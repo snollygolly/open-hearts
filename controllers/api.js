dce7797b2757b4155cd8e4b187cfde49bc4f5760
snollygolly <snollygolly@gmail.com>
2016-06-30 20:24:02 -0500

    added controller and route

---
+++ controllers/api.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const config = require("../config.json");
+
+const gameModel = require("../models/game");
+
+/**
+* newGame
+* Creates a new game, and sends the client back the information
+*
+* @param {number} players - How many players is this game for
+* @returns {object} body -  Object with status and game
+*/
+module.exports.newGame = function* newGame() {
+	const params = this.request.body;
+	if (!params.players) {
+		this.status = 400;
+		return this.body = {error: true, message: "Must include number of players"};
+	}
+	const game = gameModel.newGame(parseInt(params.players));
+	if (game.error === true) {
+		this.status = 400;
+		return this.body = {error: true, message: game.message};
+	}
+	return this.body = game;
+	// TODO: some db stuff happens here
+};
--- routes.js
+++ routes.js
@@ -8,10 +8,13 @@ const Router = require("koa-router");
 const routes = new Router();
 
 const main = require("./controllers/main.js");
+const api = require("./controllers/api.js");
 
 // routes
 
 routes.get("/", main.index);
 routes.get("/game", main.game);
+
+routes.post("/api/game", api.newGame);
 
 app.use(routes.middleware());
