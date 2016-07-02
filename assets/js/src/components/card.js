aef9970086600f1983af686053147d360c760531
Zach Shames <zlshames@gmail.com>
2016-07-02 10:02:29 -0400

    Made playing "field"

---
+++ assets/js/src/components/card.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import CardList from '../../../../models/data/cards.json';
+
+const Card = React.createClass({
+  propTypes: {
+    name: React.PropTypes.string
+  },
+  getInitialState: function () {
+    return {
+      card: null
+    };
+  },
+  componentWillMount: function () {
+    document.body.style.backgroundColor = 'darkgreen';
+
+    this.setState({
+      card: './assets/images/cards/' + CardList[0][this.props.name]
+    });
+
+    console.log(this.state.card);
+  },
+  render: function () {
+    return (
+      <div style={{ textAlign: 'center', verticalAlign: 'middle'}}>
+        <img src={ this.state.card }></img>
+      </div>
+    );
+  }
+});
+
+module.exports = Card;
--- assets/js/src/components/game.js
+++ assets/js/src/components/game.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Card from './card';
 
 const Game = React.createClass({
   propTypes: {
@@ -14,8 +15,52 @@ const Game = React.createClass({
   },
   render: function () {
     return (
-      <div>
-        <h1>HEARTS!</h1>
+      <div style={{ height: '100%', width: '100%' }}>
+        <div className="bottomHand">
+          <div className="bottomCards">
+            <Card name="2H" />
+            <Card name="2H" />
+          </div>
+        </div>
+        <div className="leftHand">
+          <div className="leftCards">
+            <Card name="CB_blue5" />
+          </div>      
+        </div>
+        <div className="topHand">
+          <div className="topCards">
+            <Card name="CB_blue5" />
+          </div>      
+        </div>
+        <div className="rightHand">
+          <div className="rightCards">
+            <Card name="CB_blue5" />
+          </div>      
+        </div>
+
+        <div className="bottomPlayArea">
+          <div className="bottomPlayCard">
+            <Card name="2H" />
+          </div>
+        </div>
+
+        <div className="leftPlayArea">
+          <div className="leftPlayCard">
+            <Card name="3H" />
+          </div>
+        </div>
+
+        <div className="topPlayArea">
+          <div className="topPlayCard">
+            <Card name="4H" />
+          </div>
+        </div>
+
+        <div className="rightPlayArea">
+          <div className="rightPlayCard">
+            <Card name="5H" />
+          </div>
+        </div>
       </div>
     );
   }
