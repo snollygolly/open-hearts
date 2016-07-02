84a412c405f0c9340e8f59e18bb79e648ac5ace6
snollygolly <snollygolly@gmail.com>
2016-07-02 14:46:44 -0500

    made front end post on button click
    fixed issue with scripts and layout

--- controllers/main.js
+++ controllers/main.js
@@ -4,12 +4,14 @@ const config = require("../config.json");
 
 module.exports.index = function* index() {
 	yield this.render("index", {
-		title: config.site.name
+		title: config.site.name,
+		script: "index"
 	});
 };
 
 module.exports.game = function* game() {
 	yield this.render("game", {
-		title: config.site.name
+		title: config.site.name,
+		script: "scripts.min"
 	});
 };
