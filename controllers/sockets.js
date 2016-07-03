6ff1c4a3981ff695a3a42295c421368e37fdbc74
snollygolly <snollygolly@gmail.com>
2016-07-03 00:19:16 -0500

    started added action performing

--- controllers/sockets.js
+++ controllers/sockets.js
@@ -31,6 +31,26 @@ io.on("fetch", co(function* co(ctx, data) {
 	io.socket.emit("fetch", JSON.stringify(game));
 }).catch(onError));
 
+io.on("action", co(function* co(ctx, data) {
+	let game = yield db.getGame(data.id);
+	if (game.error === true) {
+		// something went wrong during load
+		console.log("Something went wrong during game retrieval");
+		io.socket.emit("action", JSON.stringify(game));
+		return;
+	}
+	game = model.processAction(data.action, game);
+	if (game.error === true) {
+		// something went wrong during load
+		console.log("Something went wrong during action performing");
+		io.socket.emit("action", JSON.stringify(game));
+		return;
+	}
+	io.socket.emit("action", JSON.stringify(game));
+	// TODO: don't actually send out the entire game object, send a pruned version
+	io.broadcast("action", game);
+}).catch(onError));
+
 function onError(err) {
 	// log any uncaught errors
 	// co will not throw any errors you do not handle!!!
