fbe8296cda609d45a6c5b768ac65b254389a2cc5
snollygolly <snollygolly@gmail.com>
2016-07-03 22:38:31 -0500

    added join event to socket controller

--- controllers/sockets.js
+++ controllers/sockets.js
@@ -9,7 +9,6 @@ const session = require("../helpers/session");
 const co = require("co");
 
 io.on("connection", co.wrap(function* co(ctx, data) {
-	const players = yield session.connectPlayer(data.id);
 	io.broadcast("connect", players);
 	console.log("join event fired", data);
 }));
@@ -20,8 +19,30 @@ io.on("disconnect", co.wrap(function* co(ctx, data) {
 	console.log("leave event fired", data);
 }));
 
+io.on("join", co.wrap(function* co(ctx, data) {
+	// replace everything but letters, numbers, and spaces
+	const cleanedName = data.replace(/[^a-zA-Z0-9 ]/g, "");
+	const game = yield db.getGame(data);
+	if (game.error === true) {
+		// something went wrong during load
+		console.log("Something went wrong during game retrieval");
+		io.socket.emit("join", JSON.stringify(game));
+		return;
+	}
+	// try to join the game
+	const players = yield session.connectPlayer(data.id);
+	if (game.error === true) {
+		// something went wrong during load
+		console.log("Something went wrong during action performing");
+		io.socket.emit("join", JSON.stringify(game));
+		return;
+	}
+	console.log(game);
+	io.socket.emit("join", JSON.stringify(game));
+}));
+
 io.on("fetch", co.wrap(function* co(ctx, data) {
-	const game = yield db.getGame(data.id);
+	const game = yield db.getGame(data);
 	if (game.error === true) {
 		// something went wrong during load
 		console.log("Something went wrong during game retrieval");
@@ -32,7 +53,7 @@ io.on("fetch", co.wrap(function* co(ctx, data) {
 }));
 
 io.on("action", co.wrap(function* co(ctx, data) {
-	let game = yield db.getGame(data.id);
+	let game = yield db.getGame(data);
 	if (game.error === true) {
 		// something went wrong during load
 		console.log("Something went wrong during game retrieval");
